f2d639dc8da995bb8ca8b154ea18e311879f32a3
spechtisabela-cpu <mspechtisabela@gmail.com>
2025-12-04 15:07:55 -0300

    route.js

--- app/api/sensors/route.js
+++ app/api/sensors/route.js
@@ -1,24 +1,35 @@
 import { Pool } from 'pg';
 import { NextResponse } from 'next/server';
 
-// Connect using the URL provided by Supabase in Vercel Env Vars
+// Connect to Supabase
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: { rejectUnauthorized: false } // Required for Supabase
+  ssl: { rejectUnauthorized: false }
 });
 
 export async function POST(request) {
   let client;
   try {
     const body = await request.json();
-    const { lat, lng, temp, hum, mq9, mq135 } = body;
+    
+    // Destructure all the incoming data, including new gases
+    // We set defaults to 0 just in case the ESP32 misses one
+    const { 
+      lat, lng, temp, hum, mq9, 
+      mq135, // This stays as the "General/Average" value
+      co = 0, alcohol = 0, co2 = 0, toluene = 0, nh4 = 0, acetone = 0 
+    } = body;
 
     client = await pool.connect();
     
-    // Insert Data
+    // Insert Data into the new columns
     await client.query(
-      'INSERT INTO sensor_data (latitude, longitude, temp, humidity, mq9_val, mq135_val) VALUES ($1, $2, $3, $4, $5, $6)',
-      [lat, lng, temp, hum, mq9, mq135]
+      `INSERT INTO sensor_data (
+          latitude, longitude, temp, humidity, mq9_val, mq135_val,
+          mq135_co, mq135_alcohol, mq135_co2, mq135_toluene, mq135_nh4, mq135_acetone
+       ) 
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+      [lat, lng, temp, hum, mq9, mq135, co, alcohol, co2, toluene, nh4, acetone]
     );
 
     return NextResponse.json({ message: 'Data Saved' }, { status: 200 });
